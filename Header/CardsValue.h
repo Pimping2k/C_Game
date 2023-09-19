2ea7dee32e1bc1174bb03ac37930d7c60fb33b11
Pimping <auagfuiawg@mail.ru>
2023-09-19 10:16:21 +0300

    Made enums

--- Header/CardsValue.h
+++ Header/CardsValue.h
@@ -1,7 +1,21 @@
 #pragma once
 
 struct CardsValue {
-	std::int value;
+	//std::int value;
 
-	CardValue[] = { 6,7,8,9,10,11,12,13,14 }; //Сколько очков дает карта от 6 до Туза
+	//CardValue[] = { 6,7,8,9,10,11,12,13,14 }; //Сколько очков дает карта от 6 до Туза
+
+	CardsValues value;
+};
+
+enum class CardsValues {
+	6=6,
+	7,
+	8,
+	9,
+	10,
+	11,
+	12,
+	13,
+	14,
 };
